071d3dacdec67f2a5a4f7182c7e5e9b761a5e97c
steveb <steveb@spotify.com>
2019-11-11 11:26:38 +0100

    Add another example data set

--- predict.py
+++ predict.py
@@ -3,31 +3,21 @@ from monte_predictor.visuals import graph_example_histories, graph_frequencies
 
 print("Predicting...\n\n")
 
-# The velocities for the previous N time periods (could be days, sprints, weeks etc.)
-# The velocities could be in story points, number of stories or another unit as
-# long as the unit is the same.
-# In this case the total work was 24 over 7 time periods
-previous_velocities = [2, 4, 5, 2, 3, 5, 3]
-
-# An example of a less consistent team
-# previous_velocities = [1, 3, 8, 1, 2, 6, 3]
+# Some point per sprint velocities
+previous_velocities = [81, 18, 48, 38, 10, 47, 38]
 
 
 model = TeamModel(
     # Use the previous data we have for how quickly the team works
     sample_velocities=previous_velocities,
-    # Now assume the team creates up to about half as many stories again whilst working
-    work_split_range=(1, 1.5)
+    # Now assume the team actually does 1 - 3 times the estimated work
+    work_split_range=(1, 3)
 )
 
-# Lets make a prediction on how long it would take us to do a piece of work
-# the team estimated at 19 points. Given we've modeled the team often split
-# out extra stories between 1 and 1.5 times the initial estimate
-# we'd expect the team to complete about 24 points
-# therefor the model should give predictions close to the initial
-# data (7 time periods)
+# Lets make a prediction on how long it would take to do a task that's initially broken down
+# as 164 points
 prediction = make_a_prediction(
-    work_to_do=19,
+    work_to_do=164,
     model=model,
     simulation_count=10_000  # Running 10k predictions should be quick but accurate
 )
